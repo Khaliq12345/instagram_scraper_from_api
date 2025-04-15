b482e81f12c4dcc6901052ab631bd3c7c5d875eb
Hafiz <abdulmoustapha64@@gmail.com>
2025-04-15 17:27:29 +0100

    First Commit

---
+++ app.py
@@ -0,0 +1,90 @@
+import os
+from io import BytesIO
+
+import pandas as pd
+from nicegui import events, ui, run, app
+
+from insta_scrap.script import process_input_dataframe
+from dotenv import load_dotenv
+from dateparser import parse
+
+load_dotenv()
+
+
+class InstaApp:
+    def __init__(self):
+        self.input_df = None
+        self.password = None
+        self.total_results = 10
+
+    async def start_bot(self):
+        with ui.element("div").classes("w-full"):
+            spinner = ui.spinner(size="lg").classes("w-full")
+        file_name = await run.cpu_bound(
+            process_input_dataframe, self.input_df, self.total_results
+        )
+        spinner.visible = False
+
+        output_df = pd.read_csv(file_name)
+        buffer = BytesIO()
+        output_df.to_csv(buffer, index=False)
+        os.remove(file_name)
+        ui.button(
+            "Download output",
+            on_click=lambda: ui.download.content(
+                buffer.getvalue(), filename="output.csv"
+            ),
+        ).classes("full-width m-5")
+
+    def handle_upload(self, e: events.UploadEventArguments):
+        byte_content = e.content.read()
+        self.input_df = pd.read_csv(BytesIO(byte_content))
+
+    def handle_login(self):
+        if self.password == os.getenv("api_key"):
+            ui.notify("Logged IN", position="top", type="positive")
+            app.storage.user["api_key"] = {
+                "value": self.password,
+                "exp": parse("now").isoformat(),
+            }
+            ui.navigate.reload()
+        else:
+            ui.notify("Wrong Credentials", position="top", type="negative")
+
+    def login(self):
+        with ui.element("div").classes("w-full justify-items-center flex-col"):
+            ui.input(
+                label="API KEY", password=True, password_toggle_button=True
+            ).classes("w-1/2 text-center m-5 flat").bind_value(self, "password")
+            ui.button("Login").classes("flat").on_click(lambda: self.handle_login())
+
+    def main(self):
+        with ui.header().classes("flex justify-center"):
+            ui.label("Instagram Follower scraper").classes("md:text-h4 text-h6")
+
+        with ui.element("div").classes("w-full"):
+            ui.upload(on_upload=self.handle_upload, auto_upload=True).props(
+                "accept=.csv flat"
+            ).classes("w-full")
+            ui.number(label="Total to scrape").bind_value(self, "total_results")
+            ui.button("Start Extracting").classes("full-width m-5").on_click(
+                self.start_bot
+            )
+
+
+@ui.page("/")
+def start_app():
+    ui.colors(primary="black")
+    insta_app = InstaApp()
+    api_key = app.storage.user.get("api_key")
+    if api_key:
+        diff = abs(parse(api_key["exp"]) - parse("now"))
+        if (api_key["value"] != os.getenv("api_key")) or (diff.days > 0):
+            insta_app.login()
+        else:
+            insta_app.main()
+    else:
+        insta_app.login()
+
+
+ui.run(host="0.0.0.0", storage_secret=os.getenv("secret_key"))
---
+++ insta_scrap/script.py
@@ -0,0 +1,134 @@
+import json
+import os
+from datetime import datetime
+from concurrent.futures import ThreadPoolExecutor, as_completed
+import pandas as pd
+import requests
+from dotenv import load_dotenv
+
+load_dotenv()
+
+
+def ecrire_ligne_resultat(file_name: str, df: pd.DataFrame):
+    if os.path.exists(file_name):
+        df.to_csv(file_name, mode="a", header=False, index=False)
+    else:
+        df.to_csv(file_name, index=False)
+
+
+def get_followers(username, start_from):
+    rapidapi_key = os.getenv("rapidapi_key")
+    try:
+        url = "https://instagram-social-api.p.rapidapi.com/v1/followers"
+        querystring = {"username_or_id_or_url": username}
+        if start_from:
+            querystring["pagination_token"] = start_from
+        headers = {
+            "x-rapidapi-key": rapidapi_key,
+            "x-rapidapi-host": "instagram-social-api.p.rapidapi.com",
+        }
+        response = requests.get(url, headers=headers, params=querystring)
+        json_data = response.json()
+        items = json_data.get("data", {})
+        token = json_data.get("pagination_token", None)
+        items = items.get("items", []) if items else []
+        return items, token
+    except Exception as e:
+        print(f"Error while getting folowers : {e}")
+        return None, None
+
+
+def get_gender(username):
+    gender_key = os.getenv("gender_key")
+    try:
+        url = "https://api.genderapi.io/api/"
+        payload = {"name": username.replace(" ", ""), "key": gender_key}
+        headers = {"Content-Type": "application/x-www-form-urlencoded"}
+        response = requests.post(url, data=payload, headers=headers)
+        return json.loads(response.text)
+    except Exception as e:
+        print(f"Error while getting gender : {e}")
+        return None
+
+
+def get_posts_count(username):
+    rapidapi_key = os.getenv("rapidapi_key")
+    try:
+        url = "https://instagram-social-api.p.rapidapi.com/v1/posts"
+        querystring = {"username_or_id_or_url": username}
+        headers = {
+            "x-rapidapi-key": rapidapi_key,
+            "x-rapidapi-host": "instagram-social-api.p.rapidapi.com",
+        }
+        response = requests.get(url, headers=headers, params=querystring)
+        json_data = response.json()
+        items = json_data.get("data", {})
+        count = items.get("count", 0) if items else 0
+        return count
+    except Exception as e:
+        print(f"Error while getting folowers : {e}")
+        return None
+
+
+def sub_process_user(user, index, file_name):
+    try:
+        post_count = get_posts_count(user["username"])
+        if post_count < 5:
+            return 0
+
+        if not user.get("full_name"):
+            return 0
+
+        gender_res = get_gender(user["full_name"])
+        if (
+            gender_res
+            and ("gender" in gender_res)
+            and ("country" in gender_res)
+        ):
+            if (gender_res["gender"] == "male") and (gender_res["country"] == "US"):
+                result = {
+                    "username": user["username"],
+                    "full name": user["full_name"],
+                    "gender": gender_res["gender"],
+                    "country": gender_res["country"],
+                }
+                df = pd.DataFrame(result, index=[0])
+                ecrire_ligne_resultat(file_name, df)
+                print(f"Une ligne ajoutée pour le lien {index + 1}")
+                return 1
+    except Exception as e:
+        print(f"Erreur avec l'utilisateur {user.get('username', '')}: {e}")
+    return 0
+
+
+def process_input_dataframe(df_source: pd.DataFrame, total_results: int):
+    already_got = 0
+    # total_results = 1000
+    file_name = f"{str(int(datetime.now().timestamp()))}.csv"
+    if df_source.empty:
+        return None
+    for index, row in df_source.iterrows():
+        username = row["username_or_url"]
+        next_start_from = None
+        while total_results > already_got:
+            print(
+                f"Username_url: {username} | total_results: {total_results} | already_got: {already_got} | start_from: {next_start_from}"
+            )
+            followers_res, next_start_from = get_followers(username, next_start_from)
+            if not followers_res:
+                break
+            print(f"Total users: {len(followers_res)}, already got: {already_got}")
+            with ThreadPoolExecutor(max_workers=5) as executor:
+                futures = []
+                for user in followers_res:
+                    futures.append(executor.submit(sub_process_user, user, index, file_name))
+
+                for future in as_completed(futures):
+                    already_got += future.result()
+            
+    print("DONE!")
+    return file_name
+
+
+# def start_insta_bot() -> str:
+# process_inpuern_dataframe(input_df)
